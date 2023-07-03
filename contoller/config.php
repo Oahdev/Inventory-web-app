0e15e3f8991d5214989ccddf38ca37bb2d4fdd95
Olamide Sodeinde <90583135+Oahdev@users.noreply.github.com>
2023-07-03 07:42:24 +0100

    Update config.php

--- contoller/config.php
+++ contoller/config.php
@@ -2,9 +2,9 @@
     class DB {
         private static function connect(){
          $host = "localhost";
-         $username = "id20969912_stocks";
-         $dbname = "id20969912_stocks";
-         $password = "27L4RXtnHA?";
+         $username = "root";
+         $dbname = "stocks";
+         $password = "";
          $pdo = new PDO("mysql:hostname=$host;dbname=$dbname;charset=UTF8",$username,$password);
          $pdo->setAttribute(PDO::ATTR_ERRMODE , PDO::ERRMODE_EXCEPTION);
          return $pdo;
